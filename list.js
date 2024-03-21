c8b6e9106d840d124a3e3ea783535150544cdd20
Bubgamer07 <148393011+Bubgamer07@users.noreply.github.com>
2024-03-20 18:23:14 -0700

    Update list.js

--- list.js
+++ list.js
@@ -1,4 +1,4 @@
-files = ["computer","buggyuncensor"]
+files = ["computer","buggyuncensor","resourcemonitorcode"]
 list = document.getElementById("list")
 for (file of files) {
   list.innerHTML += "<br><a href=\""+file+"\">"+file+"</a>"
